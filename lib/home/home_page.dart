1a0b53318b1ad1a7f05fbfe3ff68ca31492eb5dc
Wilson Santos <contato@wilsonsantos.net>
2021-04-19 16:32:39 -0300

    :sparkles: feat: Finish Class 01
    Finish class 01

---
+++ lib/home/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:DevQuiz/home/widget/appBar/app_bar_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
---
+++ lib/home/widget/appBar/app_bar_widget.dart
@@ -0,0 +1,55 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widget/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: "Olá,",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: "Will",
+                              style: AppTextStyles.titleBold,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/836093?v=4"),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                )
+              ],
+            ),
+          ),
+        );
+}
