d311e0140af2f8959bbc14adec81092237e2d2ab
Wilson Santos <contato@wilsonsantos.net>
2021-04-23 09:54:25 -0300

    :pencil2: refactor: Fix typo
    fix typo Answerd

--- lib/shared/models/quiz_model.dart
+++ lib/shared/models/quiz_model.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:ffi';
 
 import 'package:DevQuiz/shared/models/question_model.dart';
 
@@ -26,14 +25,14 @@ extension LevelExtension on Level {
 class QuizModel {
   final String title;
   final List<QuestionModel> questions;
-  final int questionAwnsered;
+  final int questionAnswered;
   final String image;
   final Level level;
 
   QuizModel({
     required this.title,
     required this.questions,
-    this.questionAwnsered = 0,
+    this.questionAnswered = 0,
     required this.image,
     required this.level,
   });
@@ -42,7 +41,7 @@ class QuizModel {
     return {
       'title': title,
       'questions': questions.map((x) => x.toMap()).toList(),
-      'questionAwnsered': questionAwnsered,
+      'questionAwnsered': questionAnswered,
       'image': image,
       'level': level.parse,
     };
@@ -53,7 +52,7 @@ class QuizModel {
       title: map['title'],
       questions: List<QuestionModel>.from(
           map['questions']?.map((x) => QuestionModel.fromMap(x))),
-      questionAwnsered: map['questionAwnsered'],
+      questionAnswered: map['questionAwnsered'],
       image: map['image'],
       level: map['level'].toString().parse,
     );
